6406ab9f33ddeeecde4838f8fb9a0dba2cddcc2e
SangminSHIM <tkdals4723@gmail.com>
2023-02-27 23:57:03 +0100

    League image displayed

--- FootballStats/FootballStats/src/main/java/com/example/FootballStats/controller/LeagueController.java
+++ FootballStats/FootballStats/src/main/java/com/example/FootballStats/controller/LeagueController.java
@@ -1,14 +1,20 @@
 package com.example.FootballStats.controller;
 
+
 import com.example.FootballStats.entity.League;
 import com.example.FootballStats.entity.StatLeagueClub;
 import com.example.FootballStats.repo.LeagueRepository;
 import com.example.FootballStats.repo.StatLeagueClubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,6 +43,19 @@ public class LeagueController {
     public List<StatLeagueClub> getLeagueAllStatsById (@PathVariable("league_id") Long league_id){
         Optional<League> league = leagueRepository.findById(league_id);
         return statLeagueClubRepository.findByLeague(league);
-
     }
+
+    // Get Image
+    @RequestMapping(path="/images/{league_name}", method = RequestMethod.GET,
+        produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable("league_name") String league_name) throws IOException {
+        var imgFile = new ClassPathResource("images/leagues/"+league_name+".png");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(bytes);
+    }
+
 }
